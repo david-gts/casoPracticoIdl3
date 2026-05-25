4e7d5189ca5bbbea1dac446799606f3a7cf61c48
David Gutierrez <davidgtsdev@gmail.com>
2026-05-25 01:10:53 -0500

    fix: solucion de etiquetas, separando js a un archivo main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
+const carrusel = document.getElementById("carrusel");
 
+    document.getElementById("next").addEventListener("click", () => {
+      carrusel.scrollBy({
+        left: 800,
+        behavior: "smooth"
+      });
+    });
+
+    document.getElementById("prev").addEventListener("click", () => {
+      carrusel.scrollBy({
+        left: -800,
+        behavior: "smooth"
+      });
+    });
